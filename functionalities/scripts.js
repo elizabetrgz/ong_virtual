a086841ba7edb2a710241d8af8eb305d513feeb5
Testing <elizabetrgz91@gmail.com>
2021-11-06 00:25:40 -0300

    added new styles to some sections to fix some details to improve the view

--- functionalities/scripts.js
+++ functionalities/scripts.js
@@ -39,5 +39,24 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+
+// -------------------------------------------------Navar function--------------------------------------//
+
+function myFunctionDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
